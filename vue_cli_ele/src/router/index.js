c8686047495aef338628ab8e1a798d4a40fd1dd8
dyyliao123 <824023448@qq.com>
2018-01-04 15:09:54 +0800

    页面一进来就是登录页面

--- vue_cli_ele/src/router/index.js
+++ vue_cli_ele/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 /*主体共用页面*/
 import MainPage from '@/components/main_page'
 import login from '@/components/login/login'
@@ -13,11 +12,11 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    	{path: '/',name: 'HelloWorld',component: HelloWorld},
-    	{path: '/login',name: 'login',component: login},					//登录页面
-			{path: '/main_page',name: '后台管理',component: MainPage,children:[
-				{path:'/setting',name:'设置',component:Content,children:[
-	    		{path:'/company_info',name:'企业资料',component:CompanyInfo}    //企业资料
+  		{ path: '*', redirect: '/login' },
+    	{	path: '/login',name: 'login',component: login	},					//登录页面
+			{	path: '/main_page',name: '后台管理',component: MainPage,children:[
+				{	path:'/setting',name:'设置',component:Content,children:[
+	    		{	path:'/company_info',name:'企业资料',component:CompanyInfo	}    //企业资料
 	    	]}
 			]},
   ]
